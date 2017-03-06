5c007ea473991b1ce347061e6bcdfdc84ba3034c
Miguel Magana <miguelc.magana@gmail.com>
2017-03-06 15:44:47 -0800

    Initial commit attempt

---
+++ src/hello_world/Assignment_1_11.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_world;
+
+/**
+ *
+ * @author usmcw
+ */
+public class Assignment_1_11 {
+    public static void main (String[] args){
+        
+        float number = 0;
+        System.out.println(number);
+        
+        System.out.println("The US population will increase each year by");
+        System.out.println((60/7.0 * 60 * 24 * 365)- (60/13.0 * 60 * 24 * 365)+ (60/45.0 * 60 * 24 * 365));
+        System.out.println("Year one, the US population will be ...");
+        System.out.println(2780097 + 312032486);
+        System.out.println("Year two, the US population will be ...");
+        System.out.println(2780097 * 2 + 312032486);
+        System.out.println("Year three, the US population will be ...");
+        System.out.println(2780097 * 3 + 312032486);
+        System.out.println("Year four, the US population will be ...");
+        System.out.println(2780097 * 4 + 312032486);
+        System.out.println("Year five, the US population will be ...");
+        System.out.println(2780097 * 5 + 312032486);
+         
+        
+
+      }
+    
+}
+    
+
---
+++ src/hello_world/Hello_World.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_world;
+
+/**
+ *
+ * @author Robert
+ */
+public class Hello_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        double pi;
+        
+        pi = (4 * (1 - (1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)+(1.0/13)));
+        
+       
+       System.out.print(pi);
+    }
+    
+}
